4c9acc0b7a46e2c3d995bf32d41e26e683484a4f
JP Kiser <johnpaulkiser@gmail.com>
2020-07-01 02:31:42 -0400

    Create basic login/registration templates

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { IonPage, IonButton } from '@ionic/react';
+import { IonPage, IonContent, IonRow, IonCol, IonRouterLink, IonIcon } from '@ionic/react';
+import { mailOutline, lockClosedOutline } from 'ionicons/icons';
+import './Login.css';
 
 class Login extends React.Component {
 
   render() {
     return ( 
       <IonPage>
-        <h1>Login Here</h1> 
-        <IonButton routerLink='/register'>Register</IonButton> 
+        <IonContent>
+          <div className='container'> 
+            <h1 className='is-size-1 has-text-weight-semibold'>Login</h1> 
+            <form className='login-form'>
+              <div className='control has-icons-left'>
+                <input className='input is-rounded is-medium ' type='text' placeholder='email' />
+                <span className="icon is-small is-left">
+                  <IonIcon icon={mailOutline}/>
+                </span>
+              </div>
+              <div className='control has-icons-left mt-2'>
+                <input className='input is-rounded is-medium' type='password' placeholder='password' />
+                <span className="icon is-small is-left">
+                  <IonIcon icon={lockClosedOutline}/>
+                </span>
+              </div>
+            </form>
+            <button className='button is-primary is-large' routerLink='/dashboard'>Login</button> 
+            <br />
+            <IonRouterLink href='/register'> Need an account? </IonRouterLink>
+          </div>
+        </IonContent>
       </IonPage>
     );
   }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,27 +1,47 @@
+
 import React from 'react';
-import { IonPage, IonButton } from '@ionic/react';
+import { IonPage, IonContent, IonRow, IonCol, IonRouterLink, IonIcon } from '@ionic/react';
+import { mailOutline, lockClosedOutline } from 'ionicons/icons';
+import './Login.css';
 
 class Register extends React.Component {
 
   render() {
     return ( 
       <IonPage>
-        <h1>Register here</h1> 
-        <IonButton routerLink='/login'>Login</IonButton> 
+        <IonContent>
+          <div className='container'> 
+            <h1 className='is-size-1 has-text-weight-semibold'>Register</h1> 
+            <form className='login-form'>
+              <div className='control has-icons-left'>
+                <input className='input is-rounded is-medium ' type='text' placeholder='email' />
+                <span className="icon is-small is-left">
+                  <IonIcon icon={mailOutline}/>
+                </span>
+              </div>
+              <div className='control has-icons-left mt-2'>
+                <input className='input is-rounded is-medium' type='password' placeholder='password' />
+                <span className="icon is-small is-left">
+                  <IonIcon icon={lockClosedOutline}/>
+                </span>
+              </div>
+              <div className='control has-icons-left mt-2'>
+                <input className='input is-rounded is-medium' type='password' placeholder='re-type password' />
+                <span className="icon is-small is-left">
+                  <IonIcon icon={lockClosedOutline}/>
+                </span>
+              </div>
+            </form>
+            <button className='button is-primary is-large' routerLink='/dashboard'>Register</button> 
+            <br />
+            <p>already have an account?
+              <IonRouterLink href='/login'> Login</IonRouterLink>
+            </p>
+          </div>
+        </IonContent>
       </IonPage>
     );
   }
 }
 
-
-
-
-
-
-
-
-
-
-
 export default Register;
-
