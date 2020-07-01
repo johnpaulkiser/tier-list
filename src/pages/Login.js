0a03e045891c7795ca53dcc43b3f0ac7a5dbb386
JP Kiser <johnpaulkiser@gmail.com>
2020-07-01 13:44:07 -0400

    Add spacing to <input> tags

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,22 +10,22 @@ class Login extends React.Component {
       <IonPage>
         <IonContent>
           <div className='container'> 
-            <h1 className='is-size-1 has-text-weight-semibold'>Login</h1> 
-            <form className='login-form'>
+            <h1 className='is-size-1 has-text-weight-semibold mb-6'>Login</h1> 
+            <form className='login-form mb-6'>
               <div className='control has-icons-left'>
                 <input className='input is-rounded is-medium ' type='text' placeholder='email' />
                 <span className="icon is-small is-left">
                   <IonIcon icon={mailOutline}/>
                 </span>
               </div>
-              <div className='control has-icons-left mt-2'>
+              <div className='control has-icons-left mt-4'>
                 <input className='input is-rounded is-medium' type='password' placeholder='password' />
                 <span className="icon is-small is-left">
                   <IonIcon icon={lockClosedOutline}/>
                 </span>
               </div>
             </form>
-            <button className='button is-primary is-large' routerLink='/dashboard'>Login</button> 
+            <button className='button is-primary is-large mb-6' routerLink='/dashboard'>Login</button> 
             <br />
             <IonRouterLink href='/register'> Need an account? </IonRouterLink>
           </div>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -11,30 +11,30 @@ class Register extends React.Component {
       <IonPage>
         <IonContent>
           <div className='container'> 
-            <h1 className='is-size-1 has-text-weight-semibold'>Register</h1> 
-            <form className='login-form'>
+            <h1 className='is-size-1 has-text-weight-semibold mb-6'>Register</h1> 
+            <form className='login-form mb-6'>
               <div className='control has-icons-left'>
                 <input className='input is-rounded is-medium ' type='text' placeholder='email' />
                 <span className="icon is-small is-left">
                   <IonIcon icon={mailOutline}/>
                 </span>
               </div>
-              <div className='control has-icons-left mt-2'>
+              <div className='control has-icons-left mt-4'>
                 <input className='input is-rounded is-medium' type='password' placeholder='password' />
                 <span className="icon is-small is-left">
                   <IonIcon icon={lockClosedOutline}/>
                 </span>
               </div>
-              <div className='control has-icons-left mt-2'>
+              <div className='control has-icons-left mt-4'>
                 <input className='input is-rounded is-medium' type='password' placeholder='re-type password' />
                 <span className="icon is-small is-left">
                   <IonIcon icon={lockClosedOutline}/>
                 </span>
               </div>
             </form>
-            <button className='button is-primary is-large' routerLink='/dashboard'>Register</button> 
+            <button className='button is-primary is-large mb-6' routerLink='/dashboard'>Register</button> 
             <br />
-            <p>already have an account?
+            <p className='has-text-grey'>already have an account?
               <IonRouterLink href='/login'> Login</IonRouterLink>
             </p>
           </div>
